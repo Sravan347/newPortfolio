2b06fe7a9737d3b74a9a99afc9e3bd116b2535d5
sravan ks <sravansreyas3@gmail.com.com>
2025-08-30 17:43:34 +0530

    install required dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '../src/components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-blue-500">
+    Hello world!
+  </h1>
+  <Button>click</Button>
+    </div>
+  )
+}
+
+export default App
